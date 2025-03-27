6084941b6163c179c3ad06bc6f49d567b27c766c
yingyih <yingyihuang2000@outlook.com>
2025-03-27 17:03:05 -0400

    upd

--- puzzles.py
+++ puzzles.py
@@ -352,7 +352,22 @@ def mul_relu_block_kernel(
 ):
     block_id_x = tl.program_id(0)
     block_id_y = tl.program_id(1)
-    # Finish me!
+    off_x = block_id_x * B0 + tl.arange(0, B0)
+    off_y = block_id_y * B1 + tl.arange(0, B1)
+    off_z = off_x[None, :] + off_y[:, None] * N0
+    
+    mask_x = off_x < N0
+    mask_y = off_y < N1
+    mask_z = mask_x[None, :] & mask_y[:, None]
+
+    x = tl.load(x_ptr + off_x, mask=mask_x)
+    y = tl.load(y_ptr + off_y, mask=mask_y)
+    z = x[None, :]*y[:, None]
+    z = tl.where(z > 0, z, 0.0)
+
+    tl.store(z_ptr+off_z, z, mask_z)
+
+
     return
 
 
